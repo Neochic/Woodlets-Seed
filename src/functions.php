df3b69b511167bf19b77a526475b7d4aa2b0e6e8
Christoph Stickel <christoph@neochic.de>
2017-03-21 23:22:59 +0100

    * added base-layout to remove requirejs script loading from alterantive layout template (since woodlets seed uses webpack now) * updated comment to match current behaviour

--- src/functions.php
+++ src/functions.php
@@ -3,7 +3,7 @@ require_once(__DIR__ . '/vendor/autoload.php');
 require_once(__DIR__ . '/services/Tgm.php');
 
 /*
- * remove wp-embed javascript, since it's included via requirejs
+ * remove wp-embed javascript, since it's included in webpack build
  */
 add_action('wp_print_scripts', function() {
     wp_deregister_script('wp-embed');
