171e58edbfc6910ea0c130269a752dffe84096ae
Christoph Stickel <christoph@neochic.de>
2017-03-23 15:10:20 +0100

    * Capitalize drive letter on windows as workaround for webpack bug https://github.com/webpack/webpack/issues/4530

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,21 @@ var ExtractTextPlugin = require("extract-text-webpack-plugin");
 var CleanCSSPlugin = require("less-plugin-clean-css");
 var AutoprefixPlugin = require("less-plugin-autoprefix");
 
+/*
+ * Capitalize drive letter on windows as workaround for
+ * webpack bug.
+ * https://github.com/webpack/webpack/issues/4530
+ */
+var outputPath = path.resolve(__dirname, 'src/js/');
+outputPath = outputPath.charAt(0).toUpperCase() + outputPath.slice(1);
+
 module.exports = {
     entry: [
         "./src/js/main.ts",
         "./src/style.less"
     ],
     output: {
-        path: __dirname+"/src/js/",
+        path: outputPath,
         filename: "main-build.js"
     },
     resolve: {
